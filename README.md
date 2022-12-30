b29bc5d644d0ede292e4b04e97694e5eea4128e8
Bander Alshammari <bander@MacBook-Pro-Bander.local>
2022-12-30 21:05:19 +0300

    wave

--- README.md
+++ README.md
@@ -3,5 +3,12 @@
 ```
 /opt/homebrew/opt/llvm/bin/clang guess_seq_arr.c -o gauss-seidal
 gcc gauss-seidal.c -o gauss-seidal
-./gauss-seidal
+./gauss-seidal 8
+```
+
+
+```
+/opt/homebrew/opt/llvm/bin/clang guess_seq_arr.c -o gauss-seidal
+gcc gauss-seidal_wave.c -o gauss-seidal_wave
+./gauss-seidal_wave 8
 ```
---
+++ gauss-seidal_wave.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define MAX_ITER 100
+#define MAX 100 // maximum value of the matrix element
+#define TOL 0.000001
+
+// Generate a random double number with the maximum value of max
+double rand_double(int max)
+{
+    return ((double)rand() / (double)(RAND_MAX)) * max;
+}
+
+// Allocate 2D matrix
+void allocate_init_2Dmatrix(double ***mat, int n, int m)
+{
+    int i, j;
+    *mat = (double **)malloc(n * sizeof(double *));
+    for (i = 0; i < n; i++)
+    {
+        (*mat)[i] = (double *)malloc(m * sizeof(double));
+        for (j = 0; j < m; j++)
+            (*mat)[i][j] = rand_double(MAX);
+    }
+}
+
+// solver
+void solver(double ***mat, int n, int m)
+{
+    double diff = 0, temp;
+    int done = 0, cnt_iter = 0, i, j, diag;
+    int diagnolas = (2 * n) - 1;
+    while (!done && (cnt_iter < MAX_ITER))
+    {
+        diff = 0;
+        // loop over diagnolas sequantioaly
+        for (diag = 1; diag < diagnolas - 1; diag++)
+        {
+            //can be paralleized, reduce diff
+            for (int d = 0; d <= diag; d++)
+            {
+                int i = d;
+                int j = abs(diag - d);
+                temp = (*mat)[i][j];
+                (*mat)[i][j] = 0.2 * ((*mat)[i][j] + (*mat)[i][j - 1] + (*mat)[i - 1][j] + (*mat)[i][j + 1] + (*mat)[i + 1][j]);
+                diff += fabs((*mat)[i][j] - temp);
+            }
+        }
+        if (diff / n / n < TOL)
+            done = 1;
+        cnt_iter++;
+    }
+
+    if (done)
+        printf("Solver converged after %d iterations\n", cnt_iter);
+    else
+        printf("Solver not converged after %d iterations\n", cnt_iter);
+}
+
+int main(int argc, char *argv[])
+{
+    int n;
+    double **a;
+
+    if (argc < 2)
+    {
+        printf("Call this program with two parameters: matrix_size communication \n");
+        printf("\t matrix_size: Add 2 to a power of 2 (e.g. : 18, 1026)\n");
+
+        exit(1);
+    }
+
+    n = atoi(argv[1]);
+    printf("Matrix size = %d \n", n);
+
+    allocate_init_2Dmatrix(&a, n, n);
+
+    // Initial operation time
+    clock_t i_exec_t = clock();
+
+    solver(&a, n, n);
+
+    // Final operation time
+    clock_t f_exec_t = clock();
+    double exec_time = (double)(f_exec_t - i_exec_t) / CLOCKS_PER_SEC;
+    printf("Operations time: %f\n", exec_time);
+
+    return 0;
+}
